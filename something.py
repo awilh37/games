cd83ac00fbc2aa11b243c75da2854a5340f53e0d
awilh37 <97398812+awilh37@users.noreply.github.com>
2024-06-13 11:27:12 +0000

    Co-authored-by: blungi-schoolacct <blungi-schoolacct@users.noreply.github.com>

---
+++ something.py
@@ -0,0 +1,8 @@
+# something simple and text based
+# runnable through the pi
+# NO MODULES
+from random import random_int as random
+x=input("Give me a number: ")
+if input == "random":
+    number=random(1,10)
+    print(number)
